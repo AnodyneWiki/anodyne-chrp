18bd4dafa0f2b53e98555fddad7b3105b5a8a5c4
Xea <0xea@autistici.org>
2026-04-30 17:15:25 +0000

    update erowid

--- erowid.rb
+++ erowid.rb
@@ -1,42 +1,37 @@
-def query_experiences(search, record)
-  url = "https://api.erowid.io/search/drug?drug=#{record["Title"]}&fuzzy=false&limit=26"
-  json_text = fetch(encode_symbols(url), "application/json")
-  if json_text
-    json_syms = JSON.parse(json_text)
-    exps = []
-    for exp in json_syms["results"]
-      if exp["drug"].downcase.include?(record["Title"].downcase)
-        exps += [ { Title: exp["title"], Author: exp["author"], Id: exp["extra"]["exp_id"] } ]
-      end
-    end
-  end
-  
-  if record["Abbreviation"] && record["Abbreviation"] != record["Title"]
-    url = "https://api.erowid.io/search/drug?drug=#{record["Abbreviation"]}&fuzzy=false&limit=26"
-    json_text = fetch(encode_symbols(url), "application/json")
-    if json_text
-      json_syms = JSON.parse(json_text)
-      for exp in json_syms["results"]
-        if exp["drug"].downcase.include?(record["Abbreviation"].downcase)
-          exps += [ { Title: exp["title"], Author: exp["author"], Id: exp["extra"]["exp_id"] } ]
-        end
-      end
-    end
-  end
+require 'httparty'
 
-  if search && search != record["Title"]
-    url = "https://api.erowid.io/search/drug?drug=#{search}&fuzzy=false&limit=26"
-    json_text = fetch(encode_symbols(url), "application/json")
-    if json_text
-      json_syms = JSON.parse(json_text)
-      for exp in json_syms["results"]
-        if exp["drug"].downcase.include?(search.downcase)
-          exps += [ { Title: exp["title"], Author: exp["author"], Id: exp["extra"]["exp_id"] } ]
-        end
-      end
-    end
+def query_experiences(record)
+  #return record
+  searches = [ record["Title"] ]
+  for abr in record["Abbreviation"]
+    searches << abr
   end
-
-  record["Erowid Experience Reports"] = exps
-  return record
+  for abr in record["Aliases"]
+    searches << abr
+  end
+  searches.uniq!
+  record["Erowid Experience Reports"] = [] if record["Erowid Experience Reports"] == nil or record["Erowid Experience Reports"].empty?
+  for s in searches
+    #url = "https://api.erowid.io/search/drug?drug=#{record["Title"]}&fuzzy=false&limit=26"
+    url = "https://methcathin.one/erw?drug=#{s}&fuzzy=false&limit=26"
+    puts url
+    json_text = HTTParty.get(encode_symbols(url))
+    puts json_text
+    json_text = HTTParty.get(encode_symbols(url))
+    next if json_text.code != 200
+    puts json_text
+    json_syms = JSON.parse(json_text.body)
+    next if json_syms.empty?
+    puts JSON.pretty_generate(json_syms)
+    
+    for exp in json_syms
+      next if not exp["drug"].include?(s.downcase)
+    end
+    record["Erowid Experience Reports"] << {
+      Title: exp["title"],
+      Author: exp["author"],
+      Id: exp["extra"]["exp_id"]
+    }
+    return record
+  end
 end
